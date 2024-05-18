8507ae8501c5921ab0b195949bbd5cd6f0b150e8
Bruno Silva <147774005+brunofilipefs96@users.noreply.github.com>
2024-05-18 16:52:37 +0100

    All Models Created
    Creation of all models

---
+++ app/Http/Controllers/QuestionnaireController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Questionnaire;
+use App\Http\Requests\StoreQuestionnaireRequest;
+use App\Http\Requests\UpdateQuestionnaireRequest;
+
+class QuestionnaireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreQuestionnaireRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Questionnaire $questionnaire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Questionnaire $questionnaire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateQuestionnaireRequest $request, Questionnaire $questionnaire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Questionnaire $questionnaire)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-//Rotas

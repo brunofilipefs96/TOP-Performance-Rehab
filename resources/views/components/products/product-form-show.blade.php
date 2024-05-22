fb2a8b6edcf8b7c36005a9c5bb52145e91c9125c
Bruno Silva <147774005+brunofilipefs96@users.noreply.github.com>
2024-05-22 18:57:10 +0100

    productsCRUDdesign
    simple tailwind design for the products crud to replicate for others cruds

--- resources/views/components/products/product-form-show.blade.php
+++ resources/views/components/products/product-form-show.blade.php
@@ -1,40 +1,39 @@
-<div class="container mt-5 glass pt-5">
-    <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-            <form method="GET" action="{{ url('products') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="mb-2">
-                    <h1> Produto {{$product->id}}</h1>
-                </div>
-                <div>
-                    <label for="name">Nome</label>
-                    <input type="text" value="{{$product->name}}" disabled>
-                    <small id="nameHelp" class="form-text text-muted">We'll never share your data with anyone
-                        else.</small>
-                </div>
+<div class="container mx-auto mt-5 pt-5 glass">
+    <div class="flex justify-center mb-4">
+        <button onclick="history.back()" class="bg-gray-500 text-white py-2 px-4 rounded-md shadow-sm hover:bg-gray-700">Voltar</button>
+    </div>
+    <div class="flex justify-center">
+        <div class="w-full max-w-lg">
+            <div>
+                <h1 class="mb-2">Produto {{$product->id}}</h1>
+            </div>
 
-                <div>
-                    <label for="quantity">Quantidade</label>
-                    <input class="form-control" type="number" value="{{$product->quantity}}" disabled>
+            @if($product->image)
+                <div class="mb-4">
+                    <label for="image" class="block">Imagem</label>
+                    <img src="{{ $product->image }}" alt="Imagem do Produto" class="mt-1 block w-full h-auto border border-gray-300 rounded-md shadow-sm">
                 </div>
+            @endif
 
-                <div>
-                    <label for="price">Preço</label>
-                    <input class="form-control" type="number" value="{{$product->price}}" disabled>
-                </div>
+            <div class="mb-4">
+                <label for="name" class="block">Nome</label>
+                <input type="text" value="{{$product->name}}" disabled class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
+            </div>
 
-                <div>
-                    <label for="details">Detalhes</label>
-                    <textarea class="form-control" type="text" value="{{$product->details}}" disabled></textarea>
-                </div>
+            <div class="mb-4">
+                <label for="quantity" class="block">Quantidade</label>
+                <input class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm" type="number" value="{{$product->quantity}}" disabled>
+            </div>
 
-                <div>
-                    <a href="/products" type="button"
-                       >Voltar</a>
-                </div>
-            </form>
+            <div class="mb-4">
+                <label for="price" class="block">Preço</label>
+                <input class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm" type="number" value="{{$product->price}}" disabled>
+            </div>
+
+            <div class="mb-4">
+                <label for="details" class="block">Detalhes</label>
+                <textarea class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm" type="text" disabled>{{$product->details}}</textarea>
+            </div>
         </div>
-        <div class="col-3"></div>
     </div>
 </div>

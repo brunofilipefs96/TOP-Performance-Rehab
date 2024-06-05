b3427dedfe95af1a8bfd3f7146a1fb2804da89d0
Bruno Silva <147774005+brunofilipefs96@users.noreply.github.com>
2024-06-05 18:46:36 +0100

    trainig Types Seeder

--- database/seeders/TrainingTypeSeeder.php
+++ database/seeders/TrainingTypeSeeder.php
@@ -13,6 +13,21 @@ class TrainingTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        TrainingType::factory()->count(10)->create();
+        $tiposTreino = [
+            ['name' => 'Cardio', 'image' => 'images/training_types/1/cardio.jpg'],
+            ['name' => 'Treino de Força', 'image' => 'images/training_types/2/forca.jpg'],
+            ['name' => 'Flexibilidade', 'image' => 'images/training_types/3/flexibilidade.jpg'],
+            ['name' => 'Pilates', 'image' => 'images/training_types/4/pilates.jpg'],
+            ['name' => 'Yoga', 'image' => 'images/training_types/5/yoga.jpg'],
+            ['name' => 'HIIT', 'image' => 'images/training_types/6/hiit.jpg'],
+            ['name' => 'CrossFit', 'image' => 'images/training_types/7/crossfit.jpg'],
+            ['name' => 'Musculação', 'image' => 'images/training_types/8/musculacao.jpg'],
+            ['name' => 'Cycling', 'image' => 'images/training_types/9/cycling.jpg'],
+            ['name' => 'Aeróbica', 'image' => 'images/training_types/10/aerobica.jpg'],
+        ];
+
+        foreach ($tiposTreino as $tipo) {
+            TrainingType::create($tipo);
+        }
     }
 }

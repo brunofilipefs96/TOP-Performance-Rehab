354b1376b103a13d734e6a2b1cd1a96376fc8c9c
Nelson Ferreira <155228809+NelsonFerreira23@users.noreply.github.com>
2024-06-06 10:43:19 +0100

    Membership views
    memberships-list finalizada,
    membership-show por terminar.

--- resources/views/components/memberships/membership-form-show.blade.php
+++ resources/views/components/memberships/membership-form-show.blade.php
@@ -5,7 +5,7 @@
                 <div class="w-full max-w-lg">
 
                     <div>
-                        <h1 class="mb-2">Matrícula {{ $membership->id }}</h1>
+                        <h1 class="mb-2 text-2xl">Matrícula {{ $membership->id }}</h1>
                     </div>
 
                     <div class="mb-4">
@@ -21,14 +21,42 @@
                     </div>
 
                     <div class="mb-4">
-                        <label for="nif" class="block">Número Contribuinte</label>
+                        <label for="cc_number" class="block">Número Contribuinte</label>
                         <input type="text" value="{{ $membership->user->cc_number }}" disabled
                                class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
                     </div>
 
-                    <div class="mb-4">
+                    <div class="mb-8">
                         <label for="birth_date" class="block">Data Nascimento</label>
                         <input type="text" value="{{ \Carbon\Carbon::parse($membership->user->birth_date)->format('d/m/Y') }}" disabled
+                               class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
+                    </div>
+
+                    <div>
+                        <h1 class="mb-2 text-xl">Morada</h1>
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="address_name" class="block">Nome</label>
+                        <input type="text" value="{{ $membership->address->name }}" disabled
+                               class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="address_street" class="block">Nome da Rua</label>
+                        <input type="text" value="{{ $membership->address->street }}" disabled
+                               class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="address_city" class="block">Cidade</label>
+                        <input type="text" value="{{ $membership->address->city }}" disabled
+                               class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="address_postal_code" class="block">Código-Postal</label>
+                        <input type="text" id="address_postal_code" value="{{ $membership->address->postal_code }}" disabled
                                class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
                     </div>
 
@@ -51,15 +79,14 @@
                         </div>
                     </div>
 
-
                     <div class="mb-4">
-                        <label for="total_trainings" class="block">Total de Treinos com Personal Trainer</label>
+                        <label for="total_trainings_supervised" class="block">Total de Treinos com Personal Trainer</label>
                         <input type="text" value="{{ $membership->total_trainings_supervised }}" disabled
                                class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
                     </div>
 
                     <div class="mb-4">
-                        <label for="total_trainings" class="block">Total de Treinos Individuais</label>
+                        <label for="total_trainings_individual" class="block">Total de Treinos Individuais</label>
                         <input type="text" value="{{ $membership->total_trainings_individual }}" disabled
                                class="mt-1 block w-full p-2 border border-gray-300 text-gray-800 rounded-md shadow-sm">
                     </div>
@@ -74,3 +101,4 @@
         </div>
     </div>
 </div>
+

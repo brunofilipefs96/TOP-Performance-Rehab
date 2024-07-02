34fa2e6f7c2ffb3e98b70961b5e600056595e0fd
Melquisedeque Marins <melck_junior@hotmail.com>
2024-07-02 19:36:29 +0100

    Dashboard Client - adjusted layout

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="text-center text-sm text-black dark:text-white/70 dark:bg-gray-800 bg-gray-50">
-    <div class="dark:text-white text-gray-400 p-6">
+<footer class="text-center text-sm text-gray-400 dark:text-white-70 dark:bg-gray-800 bg-gray-50">
+    <div class="dark:text-white text-gray-400">
         <div class="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
             <div>
                 <h3 class="text-xl font-semibold mb-4">Apoio ao Cliente </h3>
@@ -9,27 +9,22 @@
                 </div>
             </div>
             <div>
-                <h3 class="text-xl font-semibold mb-4 pr-20">Opening Hours</h3>
+                <h3 class="text-xl font-semibold mb-4 pr-20">Horário de Funcionamento</h3>
                 <ul class="space-y-2 text-gray-400 text-start">
-                    <li>Monday ---------------------------- 10 AM - 05 PM</li>
-                    <li>Tuesday ---------------------------- 10 AM - 05 PM</li>
-                    <li>Wednesday ------------------------ 10 AM - 05 PM</li>
-                    <li>Thursday --------------------------- 10 AM - 05 PM</li>
-                    <li>Friday ------------------------------ 10 AM - 02 PM</li>
-                    <li>Saturday --------------------------- 10 AM - 12 PM</li>
-                    <li>Sunday ----------------------------- Closed</li>
+                    <li>Segunda-feira a sábado -------------- 10 AM - 05 PM</li>
+                    <li>Domingo ----------------------------- Fechado</li>
                 </ul>
             </div>
             <div>
-                <h3 class="text-xl font-semibold mb-4">Get in Touch</h3>
+                <h3 class="text-xl font-semibold mb-4">Fale conosco</h3>
                 <p class="text-gray-400 mb-2">
-                    <strong>Address:</strong> 8953 South Gainsway Avenue Park Ridge, IL 60068
+                    <strong>Morada:</strong> 8953 South Gainsway Avenue Park Ridge, IL 60068
                 </p>
                 <p class="text-gray-400 mb-2">
-                    <strong>Phone:</strong> +91 345-677-554, +22 333-444-555
+                    <strong>Telefone:</strong> +91 345-677-554, +22 333-444-555
                 </p>
                 <p class="text-gray-400 mb-4">
-                    <strong>Email:</strong> info@sitename.com
+                    <strong>E-mail:</strong> info@sitename.com
                 </p>
                 <div class="flex justify-center space-x-4">
                     <a href="#" class="text-gray-400 hover:text-white"><i class="fab fa-facebook"></i></a>
--- resources/views/pages/dashboard/client.blade.php
+++ resources/views/pages/dashboard/client.blade.php
@@ -1,12 +1,79 @@
 <x-app-layout>
-    <div class="container mx-auto p-4">
-        <h1 class="text-2xl font-bold mb-4">Dashboard do Cliente</h1>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+    <div class="container mx-auto pt-5 mb-10">
+        <h1 class="text-2xl font-bold mb-4 dark:text-white text-gray-800">Novidades para si!</h1>
+        <hr class="mb-10 border-gray-400 dark:border-gray-300">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             @foreach ($products as $product)
-                <div class="bg-white dark:bg-gray-800 p-4 rounded-lg shadow">
-                    <h2 class="text-lg text-gray-700 dark:text-gray-200 font-semibold">{{ $product->name }}</h2>
-                    <p class="text-gray-700 dark:text-gray-200">{{ $product->description }}</p>
-                    <p class="text-gray-700 dark:text-gray-200 font-bold">{{ $product->price }}</p>
+                <div
+                    class="product-card dark:bg-gray-800 bg-gray-500 rounded-lg overflow-hidden shadow-md text-white select-none transform transition-transform duration-300 hover:scale-105 flex flex-col justify-between"
+                    data-name="{{ $product->name }}">
+                    <a href="{{ url('products/' . $product->id) }}" class="flex-grow">
+                        <div class="flex justify-center">
+                            @if($product->image && file_exists(public_path('storage/' . $product->image)))
+                                <img src="{{ asset('storage/'. $product->image) }}" alt="{{ $product->name }}"
+                                     class="w-full h-32 object-cover">
+                            @else
+                                <div class="w-full h-32 dark:bg-gray-600 bg-gray-300 flex items-center justify-center">
+                                    <span class="text-2xl">Sem imagem</span>
+                                </div>
+                            @endif
+                        </div>
+                        <div class="p-4 dark:bg-gray-800 bg-gray-500 flex-grow">
+                            <h3 class="text-lg font-semibold mb-2">{{ $product->name }}</h3>
+                            <div class="dark:text-gray-300 text-gray-200 mb-2 flex items-center text-md">
+                                <i class="fa-solid fa-coins w-4 h-4 mr-2"></i>
+                                <span>{{ $product->price }} €</span>
+                            </div>
+                            @can('update', $product)
+                                @if($product->quantity <= 5)
+                                    <div class="text-red-400 mb-2 flex items-center text-md">
+                                        <i class="fa-solid fa-box-open w-4 h-4 mr-2 text-red-400"></i>
+                                        <span>{{ $product->quantity }} Unidades</span>
+                                    </div>
+                                @else
+                                    <div class="dark:text-gray-300 text-gray-200 mb-2 flex items-center text-md">
+                                        <i class="fa-solid fa-box-open w-4 h-4 mr-2"></i>
+                                        <span>{{ $product->quantity }} Unidades</span>
+                                    </div>
+                                @endif
+                            @endcan
+                        </div>
+                    </a>
+                    <div class="flex justify-end items-center p-4 mt-auto space-x-2">
+                        @can('update', $product)
+                            <a href="{{ url('products/' . $product->id . '/edit') }}"
+                               class="bg-blue-600 text-white flex items-center px-2 py-1 rounded-md hover:bg-blue-500 dark:bg-gray-500 dark:hover:bg-gray-400 text-sm">
+                                <i class="fa-solid fa-pen-to-square w-4 h-4 mr-2"></i>
+                                Editar
+                            </a>
+                        @endcan
+                        @can('delete', $product)
+                            <form id="delete-form-{{$product->id}}" action="{{ url('products/' . $product->id) }}"
+                                  method="POST" class="inline text-sm">
+                                @csrf
+                                @method('DELETE')
+                                <button type="button"
+                                        class="bg-red-600 text-white flex items-center px-2 py-1 rounded-md hover:bg-red-500"
+                                        onclick="confirmDelete({{ $product->id }})">
+                                    <i class="fa-solid fa-trash-can w-4 h-4 mr-2"></i>
+                                    Eliminar
+                                </button>
+                            </form>
+                        @endcan
+                        @if(!Auth::user()->hasRole('admin'))
+                            <form id="add-cart-form-{{$product->id}}" action="{{ route('cart.addProduct') }}" method="POST"
+                                  class="inline text-sm">
+                                @csrf
+                                @method('POST')
+                                <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                <button type="submit"
+                                        class="dark:bg-lime-500 bg-blue-500 px-3 py-1 rounded-md dark:hover:bg-lime-400 hover:bg-blue-400 flex items-center">
+                                    <i class="fa-solid fa-cart-plus w-4 h-4 inline-block fill-current text-white mr-2"></i>
+                                    Adicionar
+                                </button>
+                            </form>
+                        @endif
+                    </div>
                 </div>
             @endforeach
         </div>
